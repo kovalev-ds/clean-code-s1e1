7e40203dd245268055d001301bbc9c2823b5c622
kovvdn <kovvdn@gmail.com>
2022-10-30 12:22:17 +0300

    refactor: adjust HTML and CSS formatting according to rule 1.2 in html-and-css-extended.md and fix app.js file

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button  delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='remove'
     deleteButton.appendChild(deleteButtonImg);
 
 
